be6f815749cc636c816df7d3f6d29cd3d4412b76
Roman <romantuser69@gmail.com>
2022-02-08 17:17:11 +0500

    Cycless

--- src/main/java/ru/cycles/com/cycles.java
+++ src/main/java/ru/cycles/com/cycles.java
@@ -1,17 +1,24 @@
 package ru.cycles.com;
 
 public class cycles {
-    static void showDigits(int n) {
-        int start = 1;
-        while (start <= n)
-            System.out.println(start + " ");
-        start++;
+
+
+    static void showBrackets(int n) {
+        int i;
+        for (i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print('(');
+            }
+        }
+
+        for (int j = 0; j < i + 1; i++) {
+            System.out.print(')');
+            break;
+        }
     }
 
 
-
-
     public static void main(String[] args) {
-        showDigits(7);
+        showBrackets(5);
     }
 }
