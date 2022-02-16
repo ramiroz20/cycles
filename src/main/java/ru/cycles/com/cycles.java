13225b6959c3809130b089dde364da594d1e05ea
Roman <romantuser69@gmail.com>
2022-02-16 23:18:21 +0500

    [][] Cycles

--- src/main/java/ru/cycles/com/cycles.java
+++ src/main/java/ru/cycles/com/cycles.java
@@ -1,24 +1,58 @@
 package ru.cycles.com;
 
+
+
 public class cycles {
 
 
-    static void showBrackets(int n) {
-        int i;
-        for (i = 0; i < n; i++) {
-            for (int j = 0; j < i + 1; j++) {
-                System.out.print('(');
+    static char[][] star(int n) {
+        if (n % 2 == 1) {
+            int start = 1;
+            char[][] t = new char[n][n];
+            for (int i = 0; i < n / 2; i++) {
+                for (int j = 0; j < (n - start) / 2; j++) {
+                    t[i][j] = '.';
+                }
+                for (int j = (n - start) / 2; j < start; j++) {
+                    t[i][j] = '*';
+
+                }
+                for (int j = (n - start) / 2 + start; j < n;
+                     j++) {
+                    t[i][j] = '.';
+                }
+                start++;
+
             }
         }
-
-        for (int j = 0; j < i + 1; i++) {
-            System.out.print(')');
-            break;
-        }
+        return new char[5][5];
     }
-
 
     public static void main(String[] args) {
-        showBrackets(5);
+        char[][] ans = star(5);
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                System.out.println(ans[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
