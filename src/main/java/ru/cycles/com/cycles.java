210b41b05a6847488e618d71d100374be1c0e535
Roman <romantuser69@gmail.com>
2022-02-25 21:50:19 +0500

    Cycles square

--- src/main/java/ru/cycles/com/cycles.java
+++ src/main/java/ru/cycles/com/cycles.java
@@ -1,58 +1,21 @@
 package ru.cycles.com;
 
-
-
 public class cycles {
 
 
-    static char[][] star(int n) {
-        if (n % 2 == 1) {
-            int start = 1;
-            char[][] t = new char[n][n];
-            for (int i = 0; i < n / 2; i++) {
-                for (int j = 0; j < (n - start) / 2; j++) {
-                    t[i][j] = '.';
-                }
-                for (int j = (n - start) / 2; j < start; j++) {
-                    t[i][j] = '*';
-
-                }
-                for (int j = (n - start) / 2 + start; j < n;
-                     j++) {
-                    t[i][j] = '.';
-                }
-                start++;
-
-            }
-        }
-        return new char[5][5];
-    }
-
     public static void main(String[] args) {
-        char[][] ans = star(5);
-        for (int i = 0; i < 5; i++){
-            for (int j = 0; j < 5; j++){
-                System.out.println(ans[i][j]);
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i == 0 || i == 5 || j == 0 || j == 5) {
+                    System.out.print(" *");
+                } else if (i == 1 || i == 4 || j == 1 || j == 4) {
+                    System.out.print(" *");
+                } else {
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
