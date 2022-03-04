78abafd7bc6a06664890a759b17f9ba719a96338
Roman <romantuser69@gmail.com>
2022-03-04 16:56:28 +0500

    Cycles: IntNumber

---
+++ src/main/java/ru/intNumbers/com/intNumber.java
@@ -0,0 +1,19 @@
+package ru.intNumbers.com;
+
+
+public class intNumber {
+    public static void main(String[] args) {
+        int x = -2;
+        if (x < 0)
+        System.out.println("число отрицательное");
+        if (x >= 0) {
+            System.out.println("число положительное");
+
+        }
+    }
+}
+
+
+
+
+
