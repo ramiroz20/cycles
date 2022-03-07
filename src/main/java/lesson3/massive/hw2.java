03a89230e07b9d3f0aed38926ea832e76d3ebf90
Roman <romantuser69@gmail.com>
2022-03-07 11:51:58 +0500

    Massive: lesson3.hw2

---
+++ src/main/java/lesson3/massive/hw2.java
@@ -0,0 +1,11 @@
+package lesson3.massive;
+
+public class hw2 {
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+        for (int i = 0; i < 100; i++) {
+            arr[i] = i;
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
