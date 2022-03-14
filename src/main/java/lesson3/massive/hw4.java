5f1fef21773e43c4ce2909d38e7d6d65e15da15f
Roman <romantuser69@gmail.com>
2022-03-14 20:24:46 +0500

    Massive: lesson3.hw4

--- src/main/java/lesson3/massive/hw4.java
+++ src/main/java/lesson3/massive/hw4.java
@@ -1,42 +1,42 @@
 package lesson3.massive;
 
+
+import java.util.Arrays;
+
 public class hw4 {
+
     static char[][] star(int n) {
 
         char[][] t = new char[n][n];
-        if (n % 2 == 1) {
 
-            int start = 1;
-
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    t[i][j] = '2';
+        int j;
+        for (int i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (i > j)
+                    t[i][j] = '*';
+                System.out.println(Arrays.deepToString(new char[][]{new char[]{t[i][j]}})
+                        + " " + i + " " + j);
+                if (i < j) {
+                    t[i][j] = '1';
+                }else {
+                    t[i][j] = '*';
 
                 }
-                start+=2;
-
-                for (int j = n - i - start; j < n; j++) {
-                    for ( i = j; i < n; i++) {
-                        t[i][j] = '*';
-                    }
-                    start+=2;
-                }
-            }
-        }
-
-        return t;
-
-    }
-
-    public static void main(String[] args) {
-        char[][] ans = star(5);
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(ans[i][j]);
             }
             System.out.println();
         }
-    }
 
-}
+
+            return t;
+        }
+
+        public static void main (String[]args){
+            char[][] ans = star(6);
+            for (int i = 0; i < ans.length; i++) {
+                for (int j = 0; j < ans.length; j++) {
+                    System.out.print(ans[i][j]);
+                }
+                System.out.println();
+            }
+        }
+    }
