7406d7f94f57bf02ffffcd89b06f293a4352e422
Roman <romantuser69@gmail.com>
2022-03-19 22:25:22 +0500

    Massive: lesson3.hw6

---
+++ src/main/java/lesson3/massive/hw6.java
@@ -0,0 +1,19 @@
+package lesson3.massive;
+
+public class hw6 {
+    public static class hw5 {
+        public static void main(String[] args) {
+            int[] arr = {1, 2, 5, 5, 6, 23};
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > 1) {
+                    System.out.println(arr[i] + " - Больше двух");
+                    if(arr[i] < 4){
+                        System.out.println(arr[i] + " - число меньше 5ти");
+                    }
+                }
+
+            }
+        }
+    }
+}
