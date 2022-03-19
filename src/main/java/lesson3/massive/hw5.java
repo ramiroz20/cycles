33f2dba6cf6961a214e8bfb54d7a6fcb9af98ce4
Roman <romantuser69@gmail.com>
2022-03-19 21:13:25 +0500

    Massive: lesson3.hw5

---
+++ src/main/java/lesson3/massive/hw5.java
@@ -0,0 +1,12 @@
+package lesson3.massive;
+
+public class hw5 {
+    public static void main(String[] args) {
+        int arr[] = new int [5];
+        for (int i = 0; i < arr.length;i++){
+            arr[3] = 4;
+            arr[2] = 3;
+            System.out.println(arr[i]);
+        }
+    }
+}
