f8644873c915aa97833db2d10586b6980aa0607a
Roman <romantuser69@gmail.com>
2022-05-10 13:08:31 +0500

    Beer

---
+++ src/main/java/lesson3/massive/Bee.java
@@ -0,0 +1,25 @@
+package lesson3.massive;
+
+public class Bee {
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "бутылок (бутылки)";
+
+       while (beerNum > 0) {
+           if (beerNum == 1) {
+               word = "бутылка";
+
+           }
+           System.out.println(beerNum + " " + word + " пива на стене");
+           System.out.println(beerNum + " " + word + " пива.");
+           System.out.println("Возьми одну.");
+           System.out.println("Пусти по кругу");
+           beerNum = beerNum - 1;
+           if (beerNum > 0) {
+               System.out.println(beerNum + " " + word + " пива на стене");
+           } else {
+               System.out.println("Нет бутылок пива на стене");
+           }
+        }
+    }
+}
